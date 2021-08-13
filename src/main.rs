f505d0496fa3da96dc7211e3168fd61e64ffe61a
Keony Willian Schröer <keony.schroer@gmail.com>
2021-08-13 17:02:40 -0300

    lendo arquivo e printando no stdout

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -6,7 +7,11 @@ fn main() {
     let query = &args[1];
     let filename = &args[2];
 
-    println!("{:#?}", args);
-    println!("{:#?}", query);
-    println!("{:#?}", filename);
+    println!("{:?}", query);
+    println!("{:?}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+    
+    println!("With text:\n{}", contents);
 }
